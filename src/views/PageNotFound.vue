9b5122bb053708105afc94f858f608d58b597198
Leviathun <153574902+Leviathun@users.noreply.github.com>
2026-05-14 23:43:17 +0700

    update not found page

--- src/views/PageNotFound.vue
+++ src/views/PageNotFound.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
 import { defineProps, withDefaults } from 'vue'
-const props = withDefaults(
-    defineProps<{
-        resource: string
-    }>(),
+withDefaults(
+  defineProps<{
+    resource: string
+  }>(),
     {
     resource: 'page'
     }
@@ -13,5 +13,5 @@ const props = withDefaults(
 <template>
     <h1>Page Not Found</h1>
     <h3>The {{ resource }} you're looking for is not here.</h3>
-    <router-link :to="{ name: 'event-list-view' }">Back to the home page</router-link>
+    <router-link :to="{ name: 'pokemon-list-view' }">Back to the home page</router-link>
 </template>
