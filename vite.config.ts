d507d29b8ba013902d77521cba8bfc560eac6f86
Leviathun <153574902+Leviathun@users.noreply.github.com>
2026-05-22 22:05:01 +0700

    deploy & build

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.GITHUB_PAGES ? '/Component-Passender-of-airline-Project/' : '/',
   plugins: [
     vue(),
     vueDevTools(),
