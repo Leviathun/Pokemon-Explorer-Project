61d4f405051a7c29831b7ef45d2ba076f1e2129f
Leviathun <153574902+Leviathun@users.noreply.github.com>
2026-05-22 22:22:38 +0700

    fix config

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,11 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+const repositoryName = process.env.GITHUB_REPOSITORY?.split('/')[1] ?? 'Pokemon-Explorer-Project'
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.GITHUB_PAGES ? '/Component-Passender-of-airline-Project/' : '/',
+  base: process.env.GITHUB_PAGES ? `/${repositoryName}/` : '/',
   plugins: [
     vue(),
     vueDevTools(),
